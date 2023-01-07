e764cac4fae5b165354565f9fc29e2fc59add74d
Rotem <rotem227@gmail.com>
2023-01-07 20:40:31 +0200

    Fix
    Lint

--- src/segments.ts
+++ src/segments.ts
@@ -1,4 +1,4 @@
-import Group, { Config, ConfigData, StateKey, GroupName ,Middleware } from './group';
+import Group, { Config, ConfigData, StateKey, GroupName, Middleware } from './group';
 
 interface Groups {
     [name: string]: Group;
@@ -10,66 +10,66 @@ interface PendingMiddlewares {
     },
 }
 
-export type { Config, ConfigData, StateKey, GroupName ,Middleware };
+export type { Config, ConfigData, StateKey, GroupName, Middleware };
 
 export default class Segments {
     static groups: Groups = {};
 
     static pendingMiddlewares: PendingMiddlewares = {};
 
-    static addMiddleware( key: StateKey, middleware: Middleware, group = 'default' ) {
-        if ( this.groups[ group ] ) {
-            return this.groups[ group ].addMiddleware( key, middleware );
+    static addMiddleware(key: StateKey, middleware: Middleware, group = 'default') {
+        if (this.groups[group]) {
+            return this.groups[group].addMiddleware(key, middleware);
         }
 
-        if ( ! this.pendingMiddlewares[ group ] ) {
-            this.pendingMiddlewares[ group ] = {};
+        if (!this.pendingMiddlewares[group]) {
+            this.pendingMiddlewares[group] = {};
         }
 
-        if ( ! this.pendingMiddlewares[ group ][ key ] ) {
-            this.pendingMiddlewares[ group ][ key ] = [];
+        if (!this.pendingMiddlewares[group][key]) {
+            this.pendingMiddlewares[group][key] = [];
         }
 
-        this.pendingMiddlewares[ group ][ key ].push( middleware );
-        
+        this.pendingMiddlewares[group][key].push(middleware);
+
         return () => {
-            console.log( 'The removal of the middleware should be here..' );
+            console.log('The removal of the middleware should be here..');
         };
     }
 
-    static addState( key: StateKey, value: ConfigData<any>, group = 'default' ) {
-        if ( this.groups[ group ].state.hasOwnProperty( key ) ) {
-            throw new Error( 'Segment key is already exist, please use a different key.' );
+    static addState(key: StateKey, value: ConfigData<any>, group = 'default') {
+        if (this.groups[group].state.hasOwnProperty(key)) {
+            throw new Error('Segment key is already exist, please use a different key.');
         }
-        
-        this.groups[ group ].state[ key ] = value;
-    }
-    
-    static use( key: StateKey, group = 'default' ) {
-        return this.groups[ group ].use( key );
+
+        this.groups[group].state[key] = value;
     }
 
-    static create( config: Config, group = 'default' ) { 
-        const groupPendingMiddlewares = this.pendingMiddlewares[ group ];
+    static use(key: StateKey, group = 'default') {
+        return this.groups[group].use(key);
+    }
 
-        if ( groupPendingMiddlewares ) {
-            Object.keys( config ).forEach( ( configKey ) => {
-                const configKeyPendingMiddlewares = groupPendingMiddlewares[ configKey ];
+    static create(config: Config, group = 'default') {
+        const groupPendingMiddlewares = this.pendingMiddlewares[group];
 
-                if ( configKeyPendingMiddlewares ) {
-                    if ( ! config[ configKey ].middlewares ) {
-                        config[ configKey ].middlewares = [];
+        if (groupPendingMiddlewares) {
+            Object.keys(config).forEach((configKey) => {
+                const configKeyPendingMiddlewares = groupPendingMiddlewares[configKey];
+
+                if (configKeyPendingMiddlewares) {
+                    if (!config[configKey].middlewares) {
+                        config[configKey].middlewares = [];
                     }
 
-                    config[ configKey ].middlewares = config[ configKey ].middlewares?.concat( configKeyPendingMiddlewares );
+                    config[configKey].middlewares = config[configKey].middlewares?.concat(configKeyPendingMiddlewares);
 
-                    delete groupPendingMiddlewares[ configKey ];
+                    delete groupPendingMiddlewares[configKey];
                 }
-            } );
+            });
         }
-        
-        this.groups[ group ] = new Group( config, group );
 
-        return this.groups[ group ];
+        this.groups[group] = new Group(config, group);
+
+        return this.groups[group];
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig( {
+export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -10,5 +10,5 @@ export default defineConfig( {
       formats: ['cjs', 'es'],
     },
   },
-  plugins: [ dts() ],
-} );
+  plugins: [dts()],
+});
